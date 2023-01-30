71fa61c776a021883dc8300a4e1ff25bd8a80f0d
Aomsinnumber10 <112546001+Aomsinnumber10@users.noreply.github.com>
2023-01-30 09:14:55 +0700

    Add files via upload

---
+++ 1-30-23.py
@@ -0,0 +1,12 @@
+'''total = 100
+for i in range(10):
+    answer = int(input("enter a number:"))
+    total = total - answer
+print(total)'''
+
+
+total = 10
+for i in range(10):
+    answer = int(input("enter a number:"))
+    total = total + answer
+print(total)
